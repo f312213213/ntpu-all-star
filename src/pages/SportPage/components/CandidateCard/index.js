d17dde6ea5098ea8507a396ab21758506b5393f8
david <f312213213david@gmail.com>
2022-03-05 01:33:26 +0800

    connect with firestore

--- src/pages/SportPage/components/CandidateCard/index.js
+++ src/pages/SportPage/components/CandidateCard/index.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { doc, getFirestore, updateDoc, onSnapshot } from 'firebase/firestore'
 
-const CandidateCard = ({ candidate }) => {
+const CandidateCard = ({ candidate, id, sportType }) => {
+  const [realtimeCount, setRealtimeCount] = useState(0)
+  const db = getFirestore()
+  onSnapshot(doc(db, sportType, id), (doc) => {
+    setRealtimeCount(doc.data().voteCount)
+  })
+  const vote = async () => {
+    const currentCardRef = doc(db, sportType, id)
+    await updateDoc(currentCardRef, {
+      voteCount: realtimeCount + 1
+    })
+    // window.location.reload()
+  }
   return (
-      <div className="w-full h-96 rounded flex flex-col items-center overflow-hidden shadow-lg bg-custom-200">
+      <div className="w-full h-96 rounded flex flex-col items-center  shadow-lg bg-custom-200">
         <img className="h-1/2" src={candidate.photoLink} alt={candidate.introduction} />
         <div className="p-4 flex flex-col items-center">
           <div className="font-bold text-xl">{candidate.username}</div>
           <p className="text-gray-700 text-base">
-            {candidate.introduction}
+            {candidate.introduction.length > 20 ? candidate.introduction.substring(0, 20) + '...' : candidate.introduction }
           </p>
         </div>
         <div className="px-6">
-          <button className={'border-custom-800 border-2 p-4 mx-auto rounded-xl'}>
+          <button onClick={vote} className={'border-custom-800 border-2 px-4 py-2 mx-auto rounded-xl bg-custom-600 transition hover:bg-custom-500'}>
             投給我！
           </button>
           <div className={'mt-1'}>
-            目前票數：{candidate.voteCount}
+            目前票數：{realtimeCount}
           </div>
         </div>
       </div>
--- src/pages/SportPage/index.js
+++ src/pages/SportPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, createRef } from 'react'
 import { getFirestore, collection, getDocs } from 'firebase/firestore'
 
 import CandidateCard from './components/CandidateCard'
@@ -6,27 +6,44 @@ import { useParams } from 'react-router-dom'
 
 const SportPage = () => {
   const [candidates, setCandidates] = useState([])
-
+  const [copy, setCopy] = useState([])
   const { sportType } = useParams()
+  const searchRef = createRef()
+
   const getData = async () => {
+    setCandidates([])
+    const tempArray = []
     const db = getFirestore()
     const querySnapshot = await getDocs(collection(db, sportType))
     querySnapshot.forEach((doc) => {
-      setCandidates([...candidates, doc.data()])
+      tempArray.push({ id: doc.id, data: doc.data() })
     })
+    setCandidates(tempArray)
+    setCopy(tempArray)
   }
 
   useEffect(() => {
     getData()
   }, [])
 
+  const changeHandler = () => {
+    if (searchRef.current.value === '') {
+      setCandidates(copy)
+    } else {
+      const searchInfo = copy.filter((candidate) => candidate.data.username.indexOf(searchRef.current.value) !== -1)
+      setCandidates(searchInfo)
+    }
+  }
+
   return (
-      <div className={'Page'}>
+      <div className={'w-full min-h-screen pt-20 pb-5 bg-custom-500'}>
+        <div className={'w-full flex justify-center my-4'}>
+          <input placeholder={'輸入想找的名字'} onChange={changeHandler} ref={searchRef} type="text" className={'p-2 rounded outline-0 ring-4 ring-custom-400 focus:ring-red-700'}/>
+        </div>
         <div className={'w-full grid grid-cols-1 md:grid-cols-3 gap-6 px-4'}>
           {
-            candidates.map((c, index) => <CandidateCard key={index} candidate={c}/>)
+            candidates.map((c) => <CandidateCard sportType={sportType} key={c.id} id={c.id} candidate={c.data}/>)
           }
-
         </div>
       </div>
   )
